76816905103b556f90f44c0d030b5f1a11c54787
denuk987 <denuk987@gmail.com>
2019-07-29 11:16:40 +0300

    Add Metrics case class

---
+++ src/main/scala/com/github/grinko/entities/Metrics.scala
@@ -0,0 +1,21 @@
+package com.github.grinko.entities
+
+case class Metrics(INCIDENT_NUMBER: String,
+                   OFFENSE_CODE: String,
+                   OFFENSE_CODE_GROUP: String,
+                   OFFENSE_DESCRIPTION: String,
+                   DISTRICT: String,
+                   REPORTING_AREA: String,
+                   SHOOTING: String,
+                   OCCURRED_ON_DATE: String,
+                   YEAR: String,
+                   MONTH: String,
+                   DAY_OF_WEEK: String,
+                   HOUR: String,
+                   UCR_PART: String,
+                   STREET: String,
+                   Lat: String,
+                   Long: String,
+                   Location: String) {
+
+}
