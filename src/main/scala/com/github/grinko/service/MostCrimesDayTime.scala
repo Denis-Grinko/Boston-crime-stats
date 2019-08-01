28a1ae0221d459e2a608a81505ab5125ba3ac253
denuk987 <denuk987@gmail.com>
2019-08-01 14:24:32 +0300

    rename methods calc to apply in MostCrimesDayTime

--- src/main/scala/com/github/grinko/service/MostCrimesDayTime.scala
+++ src/main/scala/com/github/grinko/service/MostCrimesDayTime.scala
@@ -3,7 +3,7 @@ package com.github.grinko.service
 import com.github.grinko.entities.Metrics
 
 class MostCrimesDayTime {
-  def calc(metrics: List[Metrics]): Int = {
+  def apply(metrics: List[Metrics]): Option[Int] = {
     metrics.groupBy(_.hour).map(elem => (elem._1, elem._2.size)).toMap.maxBy(_._2)._1
   }
 }
