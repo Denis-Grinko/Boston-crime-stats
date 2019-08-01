6035243a24d9ebda034f6295137961d9595cc66c
Denis-Grinko <52849972+Denis-Grinko@users.noreply.github.com>
2019-08-01 14:15:10 +0300

    Add ConvertionMetricsUtil (#8)

---
+++ src/main/scala/com/github/grinko/util/ConvertionMetricsUtil.scala
@@ -0,0 +1,56 @@
+package com.github.grinko.util
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.github.grinko.entities.{Metrics, MetricsRaw}
+
+import scala.util.Try
+
+class ConvertionMetricsUtil {
+  lazy val DATE_FORMAT = "yyyy-MM-dd hh:mm:ss"
+
+  def rawMetricsToMetrics(metrics: List[MetricsRaw]): List[Metrics] = {
+     metrics.map(metric => new Metrics(
+       metric.incidentNumber,
+       stringToInt(metric.offenseCode),
+       metric.offenseCodeGroup,
+       metric.offenseDescription,
+       metric.district,
+       stringToInt(metric.reportingArea),
+       isShooting(metric.shooting),
+       stringToDate(metric.occurredOnDate, DATE_FORMAT),
+       stringToInt(metric.year),
+       stringToInt(metric.month),
+       metric.dayOfWeek,
+       stringToInt(metric.hour),
+       metric.ucrPart,
+       metric.street,
+       stringToDouble(metric.lat),
+       stringToDouble(metric.long),
+       metric.location
+     ))
+  }
+
+  def stringToInt(string: String): Option[Int] = {
+    Try(string.toInt).toOption
+  }
+
+  def stringToDouble(string: String): Option[Double] = {
+    Try(string.toDouble).toOption
+  }
+
+  def stringToDate(string: String, dateFormat: String): Date = {
+    val dateFormat = new SimpleDateFormat(dateFormat)
+    dateFormat.parse(string)
+  }
+
+  def isShooting(string: String): Boolean = {
+    if(string.isEmpty){
+      false
+    } else {
+      true
+    }
+  }
+
+}
