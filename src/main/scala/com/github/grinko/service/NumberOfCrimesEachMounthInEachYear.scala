97dd66053146c7bc0a7f8432416b94b41c061c89
Denis-Grinko <52849972+Denis-Grinko@users.noreply.github.com>
2019-08-01 15:46:16 +0300

    rename incorrect name in NumberOfCrimesEachMounthInEachYear

--- src/main/scala/com/github/grinko/service/NumberOfCrimesEachMounthInEachYear.scala
+++ src/main/scala/com/github/grinko/service/NumberOfCrimesEachMounthInEachYear.scala
@@ -3,8 +3,9 @@ package com.github.grinko.service
 import com.github.grinko.entities.Metrics
 
 class NumberOfCrimesEachMounthInEachYear {
-  def calc(metrics: List[Metrics]): Map[Int, Map[Int, Int]] = {
+  def calc(metrics: List[Metrics]): Map[Option[Int], Map[Option[Int], Int]] = {
     metrics.groupBy(_.year)
-      .map(elem => (elem._1, elem._2.groupBy(_.month).map(elem => (elem._1, elem._2.size)).toMap)).toMap
+      .map{ case (year, metricList) =>
+        (year, metricList.groupBy(_.month).map{case (month, crimesList) => (month, crimesList.size)}.toMap)}.toMap
   }
 }
